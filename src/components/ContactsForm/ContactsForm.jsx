62e523bcf97642081f25eaa7aea235b9db0eb8d7
Oleh Kovalets <olegkovalets1112003@gmail.com>
2023-10-07 21:23:23 +0300

    Add validation schema

--- src/components/ContactsForm/ContactsForm.jsx
+++ src/components/ContactsForm/ContactsForm.jsx
@@ -1,19 +1,42 @@
- import { Formik, Field, Form } from 'formik';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+
+const ContactSchema = Yup.object().shape({
+  name: Yup.string()
+      .trim()
+      .matches(
+        /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/,
+        'Invalid name format.'
+        )
+      .min(3, 'Too short contact name!')
+      .max(50, 'Too long contact name!')
+      .required('Required'),
+   number: Yup.string()
+      .trim()
+      .matches(
+        /\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/,
+        'Invalid phone number format'
+      )
+     .required('Required'),
+ });
 
 export const ContactsForm = ({ onSubmitForm }) => {
   return (
     <Formik
       initialValues={{ name: '', number: '' }}
+      validationSchema={ContactSchema}
       onSubmit={onSubmitForm}
     >
       <Form>
         <label>
           Name
           <Field name="name" autoComplete="off" placeholder="Oleh Kovalets" required />
+          <ErrorMessage name="name" />
         </label>
         <label>
           Phone
           <Field type="tel" autoComplete="off" name="number" required />
+          <ErrorMessage name="number" />
         </label>
         <button type="submit">Add contact</button>
       </Form>
